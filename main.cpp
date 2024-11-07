4802857d93eb4f457a526cd1059495ab69321415
Debian <debian@debian12.linuxvmimages.local>
2024-11-07 17:07:40 -0600

    rule of three

--- main.cpp
+++ main.cpp
@@ -3,10 +3,22 @@
 class T
 {
     public :
+        ~T()
+        {
+            std::cout << "Destructed" << std::endl;
+
+            delete[] m_buffer;
+        }
+
         // Parameterised constructor - bc it has default value, param can be omitted
         T(int size = 0) : m_size{size}, m_buffer{nullptr}
         {
             std::cout << "Param Constructed" << std::endl;
+
+            if ( m_size > 0)
+            {
+                m_buffer = new int[m_size];
+            }
         }
 
         // Copy constructor
@@ -14,17 +26,55 @@ class T
         {
             std::cout << "Copy Constructed" << std::endl;
 
-            m_buffer = other.m_buffer;
-            m_size = other.m_size;
+            if (this != &other)
+            {
+                m_size = other.m_size;
+
+                if (m_size > 0)
+                {
+                    m_buffer = new int[m_size];
+                }
+
+                for (int index = 0; index < m_size; index++)
+                {
+                    m_buffer[index] = other.m_buffer[index];
+                }
+            }
+            else
+            {
+                std::cout << "Tried to Copy Construct itself" << std::endl;
+            }
         }
 
         // Assignment operator
         T operator=(const T& other)
         {
-            std::cout << "Assignment operator called" << std::endl;
+            if (this != &other)
+            {
+                std::cout << "Assignment operator called" << std::endl;
 
-            m_size = other.m_size;
-            m_buffer = other.m_buffer;
+                // rease memory content of previous object
+                if (m_buffer != nullptr) 
+                {
+                    delete[] m_buffer;
+                }
+
+                m_size = other.m_size;
+
+                if (m_size > 0)
+                {
+                    m_buffer = new int[m_size];
+                }
+
+                for (int index = 0; index < m_size; index++)
+                {
+                    m_buffer[index] = other.m_buffer[index];
+                }
+            }
+            else
+            {
+                std::cout << "Tried to Assign itself" << std::endl;
+            }
 
             return *this;
         }
@@ -32,6 +82,15 @@ class T
         void details()
         {
             std::cout << "Pointer address : " << m_buffer << std::endl;
+            if ( m_buffer != nullptr )
+            {
+                for(int index = 0; index < m_size; index++)
+                {
+                    std::cout << m_buffer[index] << " ";
+                }
+                std::cout << std::endl;
+            }
+
             std::cout << "Array size : " << m_size << std::endl;
         }
 
@@ -46,11 +105,11 @@ int main()
     T t_2{t_1};
     T t_3{};
 
-    t_3 = t_2;
+    t_3 = t_2; // t_3{t_2}; bc return type is NOT T&
 
     t_1.details();
     t_2.details();
-    
+    t_3.details();
     
     return  0;
 }
